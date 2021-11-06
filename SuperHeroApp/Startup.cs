ee20a952bcbc03f49598deab65d65ae12286f39f
rubone <rubone02@gmail.com>
2021-11-06 11:20:28 -0600

    :construction: Inject super hero service

--- SuperHeroApp/Startup.cs
+++ SuperHeroApp/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SuperHeroApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,7 @@ namespace SuperHeroApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<ISuperHeroeService, SuperHeroeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
