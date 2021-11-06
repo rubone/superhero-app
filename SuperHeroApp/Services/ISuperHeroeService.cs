49ce45ff5e3ba269710cd4c883a4cb63054791b5
rubone <rubone02@gmail.com>
2021-11-06 11:19:10 -0600

    :construction: Adding interface for superhero service

---
+++ SuperHeroApp/Services/ISuperHeroeService.cs
@@ -0,0 +1,11 @@
+﻿using SuperHeroApp.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuperHeroApp.Services
+{
+    public interface ISuperHeroeService
+    {
+        Task<SearchResult> SearchSuperHero(string filter);
+    }
+}
