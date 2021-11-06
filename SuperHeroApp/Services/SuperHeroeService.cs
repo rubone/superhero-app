308f7bcc216327b5a6043449414978c4ca6c0547
rubone <rubone02@gmail.com>
2021-11-06 11:19:52 -0600

    :construction: Implementing search method

---
+++ SuperHeroApp/Services/SuperHeroeService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using SuperHeroApp.Entities;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SuperHeroApp.Services
+{
+    public class SuperHeroeService : ISuperHeroeService
+    {
+        protected readonly IConfiguration _configuration;
+
+        private readonly string _baseUrl;
+
+        private readonly string _token;
+
+        private readonly string _baseAddress;
+
+        public SuperHeroeService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _baseUrl = _configuration.GetSection("SuperHeroAPI").GetSection("BaseURL").Value;
+            _token = _configuration.GetSection("SuperHeroAPI").GetSection("Token").Value;
+            _baseAddress = $"{_baseUrl}/{_token}/";
+        }
+
+        public Task<SearchResult> SearchSuperHero(string filter)
+        {
+            var client = new HttpClient();
+
+            string searchEndpoint = _configuration.GetSection("SuperHeroAPI").GetSection("Endpoints").GetSection("Search").Value;
+
+            client.BaseAddress = new Uri(_baseAddress);
+
+            var response = client.GetAsync($"{searchEndpoint}/{filter}");
+            response.Wait();
+
+            var result = response.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<SearchResult>();
+                readTask.Wait();
+
+                return Task.FromResult(readTask.Result);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
