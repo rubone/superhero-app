e4cab09be1366821b4f13eb20d5854c25cc02254
rubone <rubone02@gmail.com>
2021-11-06 14:52:47 -0600

    :construction: Call function for search

--- SuperHeroApp/Controllers/HomeController.cs
+++ SuperHeroApp/Controllers/HomeController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SuperHeroApp.Entities;
 using SuperHeroApp.Models;
-using System;
+using SuperHeroApp.Services;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SuperHeroApp.Controllers
 {
@@ -13,14 +12,29 @@ namespace SuperHeroApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ISuperHeroeService _superHeroeService;
+
+        public HomeController(ILogger<HomeController> logger, ISuperHeroeService superHeroeService)
         {
             _logger = logger;
+            _superHeroeService = superHeroeService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            SearchViewModel model = new SearchViewModel();
+            model.SearchResult = new SearchResult();
+            model.SearchResult.Results = new List<SuperHero>();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string filter)
+        {
+            var result = _superHeroeService.SearchSuperHero(filter);
+            SearchViewModel model = new SearchViewModel();
+            model.SearchResult = result.Result;
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
