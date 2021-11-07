118b53c4d3c073fb0b65138d573c7166a4f1f926
rubone <rubone02@gmail.com>
2021-11-07 15:19:03 -0600

    :construction: Adding controller for character detail

---
+++ SuperHeroApp/Controllers/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroApp.Services;
+
+namespace SuperHeroApp.Controllers
+{
+    [Route("[controller]")]
+    public class CharacterController : Controller
+    {
+
+        private readonly ISuperHeroeService _superHeroeService;
+
+        public CharacterController(ISuperHeroeService superHeroeService)
+        {
+            _superHeroeService = superHeroeService;
+        }
+
+        [Route("{id}")]
+        public IActionResult Index(int Id)
+        {
+            var identify = Id;
+            return View();
+        }
+    }
+}
