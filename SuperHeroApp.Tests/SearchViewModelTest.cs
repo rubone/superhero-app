5d69d5c5f456b4f5e2429c1b50b743443f008492
rubone <rubone02@gmail.com>
2021-11-08 21:28:17 -0600

    :white_check_mark: Adding test project

---
+++ SuperHeroApp.Tests/SearchViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using SuperHeroApp.Models;
+using Xunit;
+
+namespace SuperHeroApp.Tests
+{
+    public class SearchViewModelTest
+    {
+        [Fact]
+        public void FilterShouldBeEqualToSearchViewModelFilter()
+        {
+            //Arrange
+            var filter = "batman";
+
+            //Act
+            SearchViewModel searchViewModel = new SearchViewModel
+            {
+                Filter = "batman",
+                SearchResult = null,
+            };
+            
+            //Assert
+            Assert.Equal(filter, searchViewModel.Filter);
+        }
+
+    }
+}
---
+++ SuperHeroApp.Tests/SuperHeroServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SuperHeroApp.Tests
+{
+    public class SuperHeroServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
